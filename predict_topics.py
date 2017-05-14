f96675c396e3e47c3c047f2813906710a349a0e6
Chris Riederer <christopher.riederer@gmail.com>
2017-05-14 04:00:47 -0400

    scripts update, dont put giant model in

--- predict_topics.py
+++ predict_topics.py
@@ -8,11 +8,9 @@ from os.path import isfile, join
 import pickle
 
 from dplython import *
-from gensim import corpora, models
-import nltk
-from nltk.corpus import stopwords
+from gensim import corpora, models, utils
 import pandas as pd
-from sklearn import linear_model, feature_extraction, cross_validation, metrics
+from sklearn import linear_model, ensemble, feature_extraction, cross_validation, metrics
 
 
 label_df = DplyFrame(pd.read_csv("supervised_topics.csv"))
@@ -22,13 +20,16 @@ label_df = DplyFrame(pd.read_csv("supervised_topics.csv"))
 # dictionary = corpora.Dictionary.load("hn_dictionaryMay13_2152.pkl")
 # lda = models.LdaModel.load("model_100topics_10passMay13_2159.gensim")
 
-dictionary = corpora.Dictionary.load("hn_dictionaryMay14_0005.pkl")
-lda = models.LdaModel.load("model_100topics_10passMay14_0018.gensim")
+# dictionary = corpora.Dictionary.load("hn_dictionaryMay14_0005.pkl")
+# lda = models.LdaModel.load("model_100topics_10passMay14_0018.gensim")
+
+dictionary = corpora.Dictionary.load("hn_dictionaryMay14_0240.pkl")
+lda = models.LdaModel.load("model_100topics_10passMay14_0259.gensim")
 
 
 def label_article(text, trained_model):
   text = text.lower()
-  tokens = nltk.word_tokenize(text)
+  tokens = list(utils.tokenize(text))
   bow = dictionary.doc2bow(tokens)
   return trained_model[bow]
 
@@ -65,41 +66,41 @@ for label, story_id in zip(label_df.topic, label_df.id):
     story_id_to_topics[story_id] = topic_dict
     print(label)
 
-df = DplyFrame(pd.DataFrame({"labels": labels, 
-                             "story_id": story_ids, 
-                             "features": topic_dicts}))
 
 labels = np.array(labels)
 dict_vectorizer = feature_extraction.DictVectorizer()
 
 story_ids2 = np.array(list(story_id_to_topics.keys()))
-# data = dict_vectorizer.fit_transform(story_id_to_topics.values())
 data = np.array([[tdict.get(i, 0) for i in range(lda.num_topics)]
                   for tdict in story_id_to_topics.values()])
 
-# results = []
-# # lr_models = {}
-# for label in set(labels):
-#   positive_story_ids = set(df >> sift(X["labels"] == label) >> X.story_id.values)
-#   y_ = np.array([s in positive_story_ids for s in story_ids2])
-#   X_ = data
-#   lr = linear_model.LogisticRegression(C=C)
-#   cv_score = cross_validation.cross_val_score(
-#     lr, X_, y_, cv=10, scoring="roc_auc").mean()
-#   lr = lr.fit(X_, y_)
-#   # lr_models[label] = lr
-#   probs = lr.predict_proba(X_)[:, 1]
-#   results.append({"alg": "log reg", "label": label, "auc": cv_score})
-#   print(C, label, cv_score, len(probs[probs > 0.19]), Counter(labels == label))
-#   print()
-# results_df = pd.DataFrame(results)
+results = []
+lr_models = {}
+for label in set(labels):
+  positive_story_ids = set(df >> sift(X["labels"] == label) >> X.story_id.values)
+  y_ = np.array([s in positive_story_ids for s in story_ids2])
+  X_ = data
+  lr = linear_model.LogisticRegression(C=C)
+  print(label, Counter(y_))
 
-# lr_fname = makeTimeFilename("models/logistic_models_", ".pkl")
-# print("writing file", lr_fname)
-# with open(lr_fname, "wb") as f:
-#   pickle.dump(lr_models, f, protocol=2)
+  cv_score = cross_validation.cross_val_score(
+    lr, X_, y_, cv=10, scoring="roc_auc").mean()
+  
+  lr = lr.fit(X_, y_)
+  lr_models[label] = lr
+  probs = lr.predict_proba(X_)[:, 1]
+  results.append({"alg": "log reg", "label": label, "auc": cv_score})
+  print(C, label, cv_score, len(probs[probs > 0.19]), Counter(labels == label))
+  print()
+results_df = pd.DataFrame(results)
+
+lr_fname = makeTimeFilename("models/logistic_models_", ".pkl")
+print("writing file", lr_fname)
+with open(lr_fname, "wb") as f:
+  pickle.dump(lr_models, f, protocol=2)
 
 
+results = []
 rf_models = {}
 for label in set(labels):
   positive_story_ids = set(df >> sift(X["labels"] == label) >> X.story_id.values)
@@ -107,19 +108,20 @@ for label in set(labels):
   X_ = data
   rf = ensemble.RandomForestClassifier(n_estimators=200)
 
-  # cv_score = cross_validation.cross_val_score(
-  #   rf, X_, y_, cv=10, scoring="roc_auc").mean()
+  cv_score = cross_validation.cross_val_score(
+    rf, X_, y_, cv=10, scoring="roc_auc").mean()
 
   rf.fit(X_, y_)
   rf_models[label] = rf
-  # results.append({"alg": "random forest", "label": label, "auc": cv_score})
-  # print(label, cv_score)
-  print(label, "done")
+  this_output = {"alg": "random forest", "label": label, "auc": cv_score}
+  print(this_output)
+  results.append(this_output)
   print()
+results_df2 = pd.DataFrame(results)
+
 
 rf_fname = makeTimeFilename("models/randomforest_models", ".pkl")
 print("writing random forest file", rf_fname)
 with open(rf_fname, "wb") as f:
   pickle.dump(rf_models, f, protocol=2)
-
 
--- utils.py
+++ utils.py
@@ -5,7 +5,9 @@ def examine_articles(text_tagger, articles, num_articles=50, num_char=200):
       print(text_tagger.text_to_tags(article_text))
       print()
 
-topic_info = [" ".join([str(x[0]) for x in lda.show_topic(i)]) for i in range(100)]
+
+
+topic_info = [" ".join([str(i)] + [str(x[0]) for x in lda.show_topic(i)]) for i in range(100)]
 def analyze_lr_model(lr, lda):
   coefs = lr.coef_[0]
   coefs_and_labels = sorted(list(zip(coefs, topic_info)), key=lambda x: -x[0]**2)
@@ -13,3 +15,20 @@ def analyze_lr_model(lr, lda):
 
 def print_analyze(lr, lda, n=100):
   for i in analyze_lr_model(lr, lda)[:n]: print(i)
+
+def print_lr_all(lr_models, n=100):
+  for k, v in lr_models.items():
+    print(k)
+    print_analyze(v, "", n=n)
+    print()
+
+
+def label_article(text, trained_model):
+  text = text.lower()
+  tokens = list(utils.tokenize(text))
+  bow = dictionary.doc2bow(tokens)
+  return trained_model[bow]
+
+
+def article_to_dict(text, trained_model):
+  return {topic: weight for topic, weight in label_article(text, trained_model)}
